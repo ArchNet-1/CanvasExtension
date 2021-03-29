d09bfaad524e392bbbcbc885e282a0ea8f2b5f47
Archaons <louispakel@gmail.com>
2021-03-29 22:20:11 +0200

    First Commit

---
+++ Editor/Scripts/CanvasExtension.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace ArchNet.Extension.ReverseMask2D
+{
+	/// <summary>
+	/// [EXTENSION] - [ARCH NET] - [CANVAS] Canvas Extension Main Script
+	/// author : Emma Mousson
+	/// </summary>
+	public static class CanvasExtension
+	{
+		#region Public Methods
+
+		/// <summary>
+		/// Activate override sorting of fiven canvas and set his sorting order to max value
+		/// </summary>
+		/// <param name="pCanvas"></param>
+		public static void SetCanvasToForeground(Canvas pCanvas)
+		{
+			pCanvas.overrideSorting = true;
+			pCanvas.sortingOrder = 32767; // Biggest value, always in first plan
+		}
+
+		/// <summary>
+		/// Reset sorting order of given canvas to 0 and stop overriding sorting
+		/// </summary>
+		/// <param name="pCanvas"></param>
+		public static void StopOverridingCanvas(Canvas pCanvas)
+		{
+			pCanvas.sortingOrder = 0;
+			pCanvas.overrideSorting = false;
+		}
+		#endregion
+	}
+}
